4531c78b350234103828758b9b1c3eb743b54e6c
github username <github email address>
2020-03-24 12:37:03 -0700

    Make server works

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_190622) do
+ActiveRecord::Schema.define(version: 2020_03_23_225237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,23 +23,20 @@ ActiveRecord::Schema.define(version: 2020_03_24_190622) do
     t.string "photo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "storage_id"
   end
 
   create_table "rooms", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
   end
 
   create_table "storages", force: :cascade do |t|
+    t.string "name"
     t.string "size"
     t.string "photo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
-    t.integer "room_id"
   end
 
   create_table "users", force: :cascade do |t|
