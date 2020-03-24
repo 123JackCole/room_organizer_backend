1701df12affc2f9a0bf9271b186d0bcf5888cbcc
123JackCole <123jack.cole@gmail.com>
2020-03-24 14:43:37 -0700

    added to iterators to serializers

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :rooms
+
+  def rooms
+    self.object.rooms.map do |room|
+      {name: room.name}
+    end 
+  end 
+
 end
