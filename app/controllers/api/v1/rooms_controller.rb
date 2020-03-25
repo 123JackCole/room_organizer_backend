e1b981af8d4790db1ccbe679161765d3d3c9d2c3
github username <github email address>
2020-03-24 18:04:39 -0700

    room crud

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -1,2 +1,42 @@
 class Api::V1::RoomsController < ApplicationController
+    
+    def index
+        rooms = Room.all
+        render json: {room: room}
+    end
+
+    def show
+        rooms = Room.find(params[:id])
+        render json: room
+    end
+
+    def new
+        rooms = Room.new
+    end
+
+    def create
+        rooms = Room.create(room_params)
+    end
+
+    def edit
+        rooms = Room.find(params[:id])
+    end
+
+    def update
+        rooms = Room.find(params[:id])
+    end
+
+    def destroy
+        user = current_user
+        session.delete(:room_id)
+        rooms.destroy
+       
+    end
+
+    private
+
+    def room_params
+        params.require(:room).permit(:name)
+    end
+
 end
